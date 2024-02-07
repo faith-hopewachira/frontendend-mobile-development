509774a0910ba4d953728a362a8c22e8dc799874
Faith <wachiraafaith@gmail.com>
2024-02-07 22:08:38 +0300

    Add one file

---
+++ Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    //var lightsOn = true
+
+    //arrayOf("Tanzania", "Rwanda", "Uganda") ctrl + /
+
+    var fullName = "Faith Hope"
+    println(fullName)
+    var age = "20"
+    println(age)
+    var phoneNumber = "0711303757"
+    println(phoneNumber)
+    var weightInKg = "50"
+    println(weightInKg)
+    var citizen = "Kenyan"
+    println(citizen)
+}
